bd133121ae6e1ae721e1ac7c8a48c24a8d45546e
Luke Logan <llogan382@gmail.com>
2019-02-20 14:06:12 -0500

    After running build, adds layout to front page

--- front-page.php
+++ front-page.php
@@ -19,34 +19,65 @@ get_header();
 			<div class="header-img">
 				<div class="d-flex align-items-center icon-row flex-row justify-content-between">
 					<div class="row-1-l">
-					<i class="fa fa-users" aria-hidden="true"></i>
+					<i class="fa fa-users fa-3x" aria-hidden="true"></i>
 					</div>
 					<div class="row-1-r">
-						icon
+					<i class="fa fa-car fa-3x" aria-hidden="true"></i>
+
 					</div>
 				</div>
 				<div class="d-flex align-items-center icon-row flex-row justify-content-around">
 					<div class="m-2">
-						icon
+					<i class="fa fa-key fa-3x" aria-hidden="true"></i>
 					</div>
 					<div class="m-2">
-						icon
+					<i class="fa fa-money fa-3x" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div class="d-flex align-items-center icon-row flex-row justify-content-center">
 					<div class="row-3-l">
-						icon
+					<i class="fa fa-calendar-check-o fa-3x" aria-hidden="true"></i>
 					</div>
 					<div class="row-3-r">
-						icon
+					<i class="fa fa-thumbs-o-up fa-3x" aria-hidden="true"></i>
 					</div>
 				</div>
 				<div class="d-flex align-items-center icon-row flex-row justify-content-around">
 					<div class="p-2">
-						icon
+					<i class="fa fa-check-square-o fa-3x" aria-hidden="true"></i>
 					</div>
 
 				</div>
+			</div>
+			<div class="featured-tiles">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							Narrow it Down By Lifestyle
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col">
+						Box 1
+						</div>
+						<div class="col">
+						Box 2
+						</div>
+						<div class="col">
+							Box 3
+						</div>
+
+					</div>
+					<div class="row">
+						<div class="col">
+								Check Our Inventory
+						</div>
+
+					</div>
+				</div>
+
+
 			</div>
 
 
